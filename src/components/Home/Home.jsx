41d1da0a7cd6169b66fcc1196a94f73ac601383d
himaloyash <himaloyashis98@gmail.com>
2023-12-14 18:52:58 +0600

    Named it Social Group

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,11 @@ import "./Home.css"
 const Home = () => {
     return (
         <div>
-            <div className='bg sm:w-100'>
-                <p className="  p-3"> Ok </p>
+            <div className='bg object-fit-cover position-relative sm:w-100'>
+                <p className=" position text-light"> Ok </p>
             </div>
             <dir>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo repudiandae corrupti nihil nulla explicabo quaerat adipisci sunt, fugiat earum suscipit laboriosam enim sequi aperiam libero sint non illo alias soluta ea maxime quas atque. Deserunt id, quaerat obcaecati sapiente voluptate delectus, consequuntur, iste doloribus recusandae non est? Corporis, eum velit.s</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
             </dir>
 
         </div>
