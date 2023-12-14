d8a33f7ac36b2a06b837fcbecc685d11e02162ab
himaloyash <himaloyashis98@gmail.com>
2023-12-14 15:01:38 +0600

    background image set

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = () => {
     return (
         <div>
-            <p className ="text-bg-primary p-3">OK</p>
+            <div className='bg sm:w-100'>
+                <p className="  p-3"> Ok </p>
+            </div>
+            <dir>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo repudiandae corrupti nihil nulla explicabo quaerat adipisci sunt, fugiat earum suscipit laboriosam enim sequi aperiam libero sint non illo alias soluta ea maxime quas atque. Deserunt id, quaerat obcaecati sapiente voluptate delectus, consequuntur, iste doloribus recusandae non est? Corporis, eum velit.s</p>
+            </dir>
+
         </div>
     );
 };
