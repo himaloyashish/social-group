54508bf02f42777f0e8e6190375a04da1e488968
himaloyash <himaloyashis98@gmail.com>
2023-12-15 01:15:43 +0600

    bg ok

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,10 @@ const Home = () => {
     return (
         <div>
             <div className='bg object-fit-cover position-relative sm:w-100 '>
-                <p className=" position text-light"> Ok </p>
+                <p className=" position text-light fs-2"> Computer Engineering
+                </p>
+                <div className='position text-light '>142, 765 computer engineer follow this page</div>
+                <p></p>
             </div>
             <dir>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
