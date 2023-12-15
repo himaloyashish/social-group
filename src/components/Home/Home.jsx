0b9c3a540d5af580a3a7e3117a17666d16869f37
himaloyash <himaloyashis98@gmail.com>
2023-12-15 23:47:11 +0600

    social will start towmorrow

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                     <Tab>Title 1</Tab>
                     <Tab>Title 2</Tab>
 
-                    <p className=' justify-content d-inline-flex '>ok</p>
+                    <p className=' justify-content d-inline-flex flex-grow-1'>ok</p>
                 </TabList>
 
                 <TabPanel>
---
+++ src/components/group/Group.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Group = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Group;
